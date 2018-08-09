1b162b235aec5e50ee043ec9af28648e871c780e
FlorianBourniquel <florianbourniquel@gmai.com>
2018-08-09 14:03:09 -0400

    Augmentation des options de mining DateMode #7

--- Mining.py
+++ Mining.py
@@ -15,6 +15,21 @@ class NumberCommitError(Exception):
     pass
 
 
+class NotEnoughOfCommitError(Exception):
+    """Number of Commit exception"""
+    pass
+
+
+class WrongArgument(Exception):
+    """Wrong argument exception"""
+    pass
+
+
+class NoModeSpecified(Exception):
+    """No mode specified exception"""
+    pass
+
+
 def url_validator(url):
     try:
         result = urlparse(url)
@@ -29,7 +44,13 @@ parser.add_argument("path", help="Path where clones will be store.")
 parser.add_argument("n", help="Number of clone", type=int)
 parser.add_argument("-file", help="if url is a txt that contains all android project url (one url per line)"
                     , action="store_true")
-parser.add_argument("step", help="Step of commit between each clone", type=int)
+parser.add_argument("-commitMode", help="if you want to work with commit"
+                    , action="store_true")
+parser.add_argument("-dateMode", help="if you want to work with date"
+                    , action="store_true")
+parser.add_argument("-releaseMode", help="if you want to work with release"
+                    , action="store_true")
+parser.add_argument("step", help="Step of commit/day/release between each clone", type=int)
 args = parser.parse_args()
 
 if args.file and not args.url.endswith(".txt"):
@@ -46,7 +67,65 @@ else:
 shutil.rmtree(path + "tmp", ignore_errors=True)
 
 
-def clone(url):
+def clone_commit(url):
+    if not url_validator(url):
+        raise URLError("url not correct")
+    number = len(os.walk(path).__next__()[1])
+    repo = Repo.clone_from(url, path + "tmp")
+    commits = list(repo.iter_commits())
+    if len(commits) < args.n * args.step:
+        raise NumberCommitError("Not enough commit with the specified args")
+    commits.clear()
+    for i in range(number, number + args.n):
+        tmp_list = list(repo.iter_commits(max_count=1, skip=args.step * (i - number)))
+        commits.append(tmp_list)
+        project_name = url.replace("https://github.com/", "").replace(".git", "").replace("/", "_")
+        f = open(path + "tmp/" + project_name + ".projectName", "w+")
+        f.close()
+        shutil.copytree(path + "tmp", path + str(i + 1))
+        tmp_repo = Repo(path + str(i + 1))
+        git_obj = tmp_repo.git
+        git_obj.checkout(tmp_list[0].hexsha)
+    shutil.rmtree(path + "tmp", ignore_errors=True)
+
+
+def clone_date(url):
+    if not url_validator(url):
+        raise URLError("url not correct")
+    number = sum(os.path.isdir(os.path.join(path, i)) for i in os.listdir(path))
+    repo = Repo.clone_from(url, path + "tmp")
+    commits = list(repo.iter_commits())
+    tmp_list = list(repo.iter_commits(max_count=1, skip=0))
+    commits.append(tmp_list)
+    project_name = url.replace("https://github.com/", "").replace(".git", "").replace("/", "_")
+    f = open(path + "tmp/" + project_name + ".projectName", "w+")
+    f.close()
+    shutil.copytree(path + "tmp", path + str(number + 1))
+    tmp_repo = Repo(path + str(number + 1))
+    git_obj = tmp_repo.git
+    git_obj.checkout(tmp_list[0].hexsha)
+    prev_date = tmp_list[0].committed_datetime
+    index = 1
+    for i in range(number+1, number + args.n):
+        while index < (len(commits)-1):
+            if (prev_date-commits[index].committed_datetime).days >= args.step:
+                project_name = url.replace("https://github.com/", "").replace(".git", "").replace("/", "_")
+                f = open(path + "tmp/" + project_name + ".projectName", "w+")
+                f.close()
+                shutil.copytree(path + "tmp", path + str(i + 1))
+                tmp_repo = Repo(path + str(i + 1))
+                git_obj = tmp_repo.git
+                git_obj.checkout(commits[index].hexsha)
+                prev_date = commits[index].committed_datetime
+                index += 1
+                break
+            index += 1
+        if index > (len(commits)-1):
+            raise NotEnoughOfCommitError
+    shutil.rmtree(path + "tmp", ignore_errors=True)
+
+
+def clone_release(url):
     if not url_validator(url):
         raise URLError("url not correct")
     number = sum(os.path.isdir(os.path.join(path, i)) for i in os.listdir(path))
@@ -68,10 +147,18 @@ def clone(url):
     shutil.rmtree(path + "tmp", ignore_errors=True)
 
 
-if args.file:
-    with open(args.url) as file:
-        [clone(line.rstrip('\n')) for line in file]
+if args.commitMode:
+    if args.file:
+        with open(args.url) as file:
+            [clone_commit(line.rstrip('\n')) for line in file]
+    else:
+        clone_commit(args.url)
+
+elif args.dateMode:
+    clone_date(args.url)
+
+elif args.releaseMode:
+    clone_release(args.url)
 
 else:
-    clone(args.url)
-
+    raise NoModeSpecified
